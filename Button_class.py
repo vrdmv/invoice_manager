7486ebc391ef673b5cd12778fd1f9c9be3f1e114
Viktor Dimov <58368947+vrdmv@users.noreply.github.com>
2020-03-06 15:39:02 +0100

    Delete Button_class.py

--- Button_class.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-class Button(QtWidgets.QPushButton):
-    def __init__(self, parent=None):
-        super(Button, self).__init__(parent)
-        font = QtGui.QFont()
-        font.setFamily("Arial")
-        font.setPointSize(9)
-        self.setFont(font)

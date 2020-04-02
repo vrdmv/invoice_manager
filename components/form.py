645d0ead58dab17eee994918b1d8ddadcbde02e9
vrdmv <viktor.dimov1@gmail.com>
2020-04-02 19:57:26 +0200

    Refactoring - added all of the QLineEdits into a list for easier access

--- components/form.py
+++ components/form.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
+from components.input import *
 
-# To be refactored
+
 class Form(QDialog):
     """Show a pop-up form for the user to input invoice data."""
     NumGridRows = 3
@@ -8,24 +9,32 @@ class Form(QDialog):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.input_1 = QLineEdit(self)
-        self.input_2 = QLineEdit(self)
-        self.input_3 = QLineEdit(self)
-        self.input_4 = QLineEdit(self)
-        self.input_5 = QLineEdit(self)
-        self.input_6 = QLineEdit(self)
-        self.input_7 = QLineEdit(self)
-        self.input_8 = QLineEdit(self)
-        self.input_9 = QLineEdit(self)
-        self.input_10 = QLineEdit(self)
-        self.input_11 = QLineEdit(self)
-        self.input_12 = QLineEdit(self)
-        self.input_13 = QLineEdit(self)
-        self.input_14 = QLineEdit(self)
-        self.input_15 = QLineEdit(self)
-        self.input_16 = QLineEdit(self)
-        self.input_17 = QLineEdit(self)
-        self.input_18 = QLineEdit(self)
+
+        self.input_list = [QLineEdit(self), QLineEdit(self), QLineEdit(self),
+                           QLineEdit(self), QLineEdit(self), QLineEdit(self),
+                           QLineEdit(self), QLineEdit(self), QLineEdit(self),
+                           QLineEdit(self), QLineEdit(self), QLineEdit(self),
+                           QLineEdit(self), QLineEdit(self), QLineEdit(self),
+                           QLineEdit(self), QLineEdit(self), QLineEdit(self)]
+
+        self.input = self.input_list[0]
+        self.input_1 = self.input_list[1]
+        self.input_2 = self.input_list[2]
+        self.input_3 = self.input_list[3]
+        self.input_4 = self.input_list[4]
+        self.input_5 = self.input_list[5]
+        self.input_6 = self.input_list[6]
+        self.input_7 = self.input_list[7]
+        self.input_8 = self.input_list[8]
+        self.input_9 = self.input_list[9]
+        self.input_10 = self.input_list[10]
+        self.input_11 = self.input_list[11]
+        self.input_12 = self.input_list[12]
+        self.input_13 = self.input_list[13]
+        self.input_14 = self.input_list[14]
+        self.input_15 = self.input_list[15]
+        self.input_16 = self.input_list[16]
+        self.input_17 = self.input_list[17]
 
         self.button_box = QDialogButtonBox(QDialogButtonBox.Ok |
                                            QDialogButtonBox.Cancel, self)
@@ -34,24 +43,24 @@ class Form(QDialog):
 
         self.formbox = QGroupBox("Please provide invoice data below: ")
         layout = QFormLayout()
-        layout.addRow(QLabel("Business name: "), self.input_1)
-        layout.addRow(QLabel("Customer name: "), self.input_2)
-        layout.addRow(QLabel("Customer email: "), self.input_3)
-        layout.addRow(QLabel("Invoice number: "), self.input_4)
-        layout.addRow(QLabel("Sent date: "), self.input_5)
-        layout.addRow(QLabel("Due date: "), self.input_6)
-        layout.addRow(QLabel("Note: "), self.input_7)
-        layout.addRow(QLabel("Item 1: "), self.input_8)
-        layout.addRow(QLabel("Item 1 Quantity: "), self.input_9)
-        layout.addRow(QLabel("Item 1 Price: "), self.input_10)
-        layout.addRow(QLabel("Item 1 Amount: "), self.input_11)
-        layout.addRow(QLabel("Subtotal: "), self.input_12)
-        layout.addRow(QLabel("Tax: "), self.input_13)
-        layout.addRow(QLabel("Discounts: "), self.input_14)
-        layout.addRow(QLabel("Total: "), self.input_15)
-        layout.addRow(QLabel("Business name repeat: "), self.input_16)
-        layout.addRow(QLabel("Business email: "), self.input_17)
-        layout.addRow(QLabel("Business phone: "), self.input_18)
+        layout.addRow(QLabel("Business name: "), self.input)
+        layout.addRow(QLabel("Customer name: "), self.input_1)
+        layout.addRow(QLabel("Customer email: "), self.input_2)
+        layout.addRow(QLabel("Invoice number: "), self.input_3)
+        layout.addRow(QLabel("Sent date: "), self.input_4)
+        layout.addRow(QLabel("Due date: "), self.input_5)
+        layout.addRow(QLabel("Note: "), self.input_6)
+        layout.addRow(QLabel("Item 1: "), self.input_7)
+        layout.addRow(QLabel("Item 1 Quantity: "), self.input_8)
+        layout.addRow(QLabel("Item 1 Price: "), self.input_9)
+        layout.addRow(QLabel("Item 1 Amount: "), self.input_10)
+        layout.addRow(QLabel("Subtotal: "), self.input_11)
+        layout.addRow(QLabel("Tax: "), self.input_12)
+        layout.addRow(QLabel("Discounts: "), self.input_13)
+        layout.addRow(QLabel("Total: "), self.input_14)
+        layout.addRow(QLabel("Business name repeat: "), self.input_15)
+        layout.addRow(QLabel("Business email: "), self.input_16)
+        layout.addRow(QLabel("Business phone: "), self.input_17)
         self.formbox.setLayout(layout)
 
         main_layout = QVBoxLayout()
@@ -60,58 +69,3 @@ class Form(QDialog):
         self.setLayout(main_layout)
         self.setMinimumWidth(400)
         self.setWindowTitle("Invoice data input")
-
-    def get_input_1(self):
-        return self.input_1.text()
-
-    def get_input_2(self):
-        return self.input_2.text()
-
-    def get_input_3(self):
-        return self.input_3.text()
-
-    def get_input_4(self):
-        return self.input_5.text()
-
-    def get_input_5(self):
-        return self.input_5.text()
-
-    def get_input_6(self):
-        return self.input_6.text()
-
-    def get_input_7(self):
-        return self.input_7.text()
-
-    def get_input_8(self):
-        return self.input_8.text()
-
-    def get_input_9(self):
-        return self.input_9.text()
-
-    def get_input_10(self):
-        return self.input_10.text()
-
-    def get_input_11(self):
-        return self.input_11.text()
-
-    def get_input_12(self):
-        return self.input_12.text()
-
-    def get_input_13(self):
-        return self.input_13.text()
-
-    def get_input_14(self):
-        return self.input_14.text()
-
-    def get_input_15(self):
-        return self.input_15.text()
-
-    def get_input_16(self):
-        return self.input_16.text()
-
-    def get_input_17(self):
-        return self.input_17.text()
-
-    def get_input_18(self):
-        return self.input_18.text()
-
